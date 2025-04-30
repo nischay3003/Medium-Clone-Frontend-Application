3c3f0cc08eb2db12539f449c50dcd252896cf805
nischay3003 <nischay728@gmail.com>
2025-04-30 19:56:06 +0530

    updated default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { Blogs } from './pages/Blogs'
@@ -10,6 +10,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+        <Route path="/" element={<Navigate to="/signup" replace />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
